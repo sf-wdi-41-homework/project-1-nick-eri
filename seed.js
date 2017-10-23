97974c73b1dabfba4a4c78769614327d95c4a5de
severejetlag <nhl1013@gmail.com>
2017-10-23 08:45:29 -0700

    updated seed file

--- seed.js
+++ seed.js
@@ -1,3 +1,4 @@
+
 var db = require("./models");
 
 var softwaresList = [
@@ -67,7 +68,48 @@ var softwaresList = [
 	}
 ]
 
-
+var usersList = [
+	{
+		username: 'severejetlag',
+		fName: 'Nick',
+		lName: 'Lee',
+		avatar: 'something.png',
+		jobTitle: 'WDI Student', 
+		jobField: 'Tech', 
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat perspiciatis commodi, ex deleniti quas nostrum odit ab officia accusantium voluptates explicabo ullam doloribus assumenda sunt eum enim ipsum id sapiente!', 
+		votes: 20
+	}, 
+	{
+		username: 'severejetlag2',
+		fName: 'Nick2',
+		lName: 'Lee',
+		avatar: 'something.png',
+		jobTitle: 'Front End Developer', 
+		jobField: 'Tech', 
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat perspiciatis commodi, ex deleniti quas nostrum odit ab officia accusantium voluptates explicabo ullam doloribus assumenda sunt eum enim ipsum id sapiente!', 
+		votes: 30
+	},
+	{
+		username: 'severejetlag3',
+		fName: 'Nick3',
+		lName: 'Lee',
+		avatar: 'something.png',
+		jobTitle: 'Designer', 
+		jobField: 'Tech', 
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat perspiciatis commodi, ex deleniti quas nostrum odit ab officia accusantium voluptates explicabo ullam doloribus assumenda sunt eum enim ipsum id sapiente!', 
+		votes: 40
+	},
+	{
+		username: 'severejetlag4',
+		fName: 'Nick4',
+		lName: 'Lee',
+		avatar: 'something.png',
+		jobTitle: 'WDI Student', 
+		jobField: 'Tech', 
+		blurb: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat perspiciatis commodi, ex deleniti quas nostrum odit ab officia accusantium voluptates explicabo ullam doloribus assumenda sunt eum enim ipsum id sapiente!', 
+		votes: 100
+	}
+]
 
 var workspaceItemsList = [
 	{
@@ -98,49 +140,53 @@ var workspaceItemsList = [
 ]
 
 
-db.Software.remove({}, function(err, users){
-	db.Software.create(softwaresList, function(err, softwares){
-		if(err) {return console.log('ERROR', err);}
-		console.log("all softwares:", softwares);
-		console.log('created', softwares.length, 'softwares');
-		db.WorkspaceItem.remove({}, function(err, workspaceItems){
-			console.log('removed all workspaceItems');
-			workspaceItemsList.forEach(function(workspaceItemData) {
-				var workspaceItem = new db.WorkspaceItem({
-		          	description: workspaceItemData.description
-				});
-		        db.User.findOne({username: workspaceItemData.username}, function (err, foundUser) {
-		          	console.log(`found user ${foundUser.username} for for workspace item ${workspaceItem.description}`);
-		          	if (err) {
-		            	console.log(err);
-		            	return;
-		          	}
-		          	workspaceItem._userId = foundUser._id;
-		          	console.log("\nUSER\N",foundUser._id)				   
-					db.Software.findOne({name: workspaceItemData.name}, function (err, foundSoftware) {
-			          	console.log(`found software ${foundSoftware.name} for for workspace item ${workspaceItem.description}`);
-			          	if (err) {
-			            	console.log(err);
-			            	return;
-			          	}
-			          	console.log("\nSOFTWARE\n",foundSoftware._id)				   
-			          	workspaceItem._softwareId = foundSoftware._id;
-			          	workspaceItem.save(function(err, saveWorkspaceItem){
-					        	if (err) {
-					          		return console.log(err);
-								}
-								console.log('saved workspace item');
-								process.exit(0);
-							});
+db.User.remove({}, function(err, users){
+  	db.User.create(usersList, function(err, users){
+	    // code in here runs after all albums are created
+	    if (err) { return console.log('ERROR', err); }
+	    console.log("all users:", users);
+	    console.log("created", users.length, "users");
+		db.Software.remove({}, function(err, users){
+			db.Software.create(softwaresList, function(err, softwares){
+				if(err) {return console.log('ERROR', err);}
+				console.log("all softwares:", softwares);
+				console.log('created', softwares.length, 'softwares');
+				db.WorkspaceItem.remove({}, function(err, workspaceItems){
+					console.log('removed all workspaceItems');
+					workspaceItemsList.forEach(function(workspaceItemData) {
+						var workspaceItem = new db.WorkspaceItem({
+				          	description: workspaceItemData.description
 						});
+				        db.User.findOne({username: workspaceItemData.username}, function (err, foundUser) {
+				          	console.log(`found user ${foundUser.username} for for workspace item ${workspaceItem.description}`);
+				          	if (err) {
+				            	console.log(err);
+				            	return;
+				          	}
+				          	workspaceItem._userId = foundUser._id;
+				          	console.log("\nUSER\N",foundUser._id)				   
+							db.Software.findOne({name: workspaceItemData.name}, function (err, foundSoftware) {
+					          	console.log(`found software ${foundSoftware.name} for for workspace item ${workspaceItem.description}`);
+					          	if (err) {
+					            	console.log(err);
+					            	return;
+					          	}
+					          	console.log("\nSOFTWARE\n",foundSoftware._id)				   
+					          	workspaceItem._softwareId = foundSoftware._id;
+					          	workspaceItem.save(function(err, saveWorkspaceItem){
+							        	if (err) {
+							          		return console.log(err);
+										}
+										console.log('saved workspace item');
+										process.exit(0);
+									});
+								});
+							});
+						
+				      	
 					});
+				});
 			});
 		});
-	});
+  	});
 });
-
-
-
-
-
-
